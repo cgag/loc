0f7a9545036b13b815579891c55cbe1b99739892
Curtis Gagliardi <curtis@curtis.io>
2016-08-26 23:17:43 -0700

    steps towards more comment configurations

--- src/lib.rs
+++ src/lib.rs
@@ -40,14 +40,21 @@ impl Count {
 }
 
 pub enum LineConfig<'a> {
-    Multi {
+    SingleMulti {
         single_start: &'a str,
         multi_start: &'a str,
         multi_end: &'a str,
     },
-    Single {
+    SingleOnly {
         single_start: &'a str,
     },
+    MultiOnly {
+        multi_start: &'a str,
+        multi_end: &'a str,
+    }, /* Everything {
+        *     singles: Vec<&'a str>,
+        *     multies: Vec<(&'a str, &'a str)>,
+        * }, */
 }
 
 // Do any languages actually use utf8 chars as comment chars?
@@ -126,23 +133,24 @@ pub fn lang_from_ext(filepath: &str) -> Lang {
 
 pub fn counter_config_for_lang<'a>(lang: &Lang) -> LineConfig<'a> {
     enum CT<'a> {
-        Single(&'a str),
-        Multi(&'a str, &'a str, &'a str),
+        SO(&'a str),
+        MO(&'a str, &'a str),
+        SM(&'a str, &'a str, &'a str),
     }
     // use self::ConfigTuple::*;
 
-    let c_style = CT::Multi("//", "/*", "*/");
-    let sh_style = CT::Single("#");
+    let c_style = CT::SM("//", "/*", "*/");
+    let sh_style = CT::SO("#");
 
     let ctuple = match *lang {
-        Lang::Haskell => CT::Multi("--", "{-", "-}"),
-        Lang::Perl => CT::Multi("#", "=pod", "=cut"),
-        Lang::INI => CT::Single(";"),
+        Lang::Haskell => CT::SM("--", "{-", "-}"),
+        Lang::Perl => CT::SM("#", "=pod", "=cut"),
+        Lang::INI => CT::SO(";"),
         // TODO(cgag): Well, some architectures use ;, @, |, etc.
         // Need a way to specify more than one possible comment char.
-        Lang::Assembly => CT::Multi("#", "/*", "*/"),
+        Lang::Assembly => CT::SM("#", "/*", "*/"),
         // TODO(cgag): Welp, single is not always necessary
-        Lang::XML => CT::Multi("GIEBBBLLLlXKJJKJK", "<!--", "-->"),
+        Lang::XML => CT::MO("<!--", "-->"),
         Lang::BourneShell | Lang::Ruby | Lang::Make | Lang::Awk => sh_style,
         // TODO(cgag): not 100% that yacc belongs here.
         Lang::C | Lang::CCppHeader | Lang::Rust | Lang::Yacc => c_style,
@@ -151,14 +159,20 @@ pub fn counter_config_for_lang<'a>(lang: &Lang) -> LineConfig<'a> {
     };
 
     match ctuple {
-        CT::Multi(single, start, end) => {
-            LineConfig::Multi {
+        CT::SM(single, start, end) => {
+            LineConfig::SingleMulti {
                 single_start: single,
                 multi_start: start,
                 multi_end: end,
             }
         }
-        CT::Single(single) => LineConfig::Single { single_start: single },
+        CT::SO(single) => LineConfig::SingleOnly { single_start: single },
+        CT::MO(start, end) => {
+            LineConfig::MultiOnly {
+                multi_start: start,
+                multi_end: end,
+            }
+        }
     }
 }
 
@@ -205,14 +219,17 @@ pub fn count(filepath: &str) -> Count {
     let lang = lang_from_ext(filepath);
     let config = counter_config_for_lang(&lang);
     match config {
-        LineConfig::Single { single_start: single } => count_mmap_unsafe_single(filepath, single),
-        LineConfig::Multi { single_start: single, multi_start, multi_end } => {
-            count_mmap_unsafe_multi(filepath, single, multi_start, multi_end)
+        LineConfig::SingleOnly { single_start: single } => count_single_only(filepath, single),
+        LineConfig::SingleMulti { single_start: single, multi_start, multi_end } => {
+            count_single_multi(filepath, single, multi_start, multi_end)
+        }
+        LineConfig::MultiOnly { multi_start: mstart, multi_end: mend } => {
+            count_multi_only(filepath, mstart, mend)
         }
     }
 }
 
-pub fn count_mmap_unsafe_single(filepath: &str, single_start: &str) -> Count {
+pub fn count_single_only(filepath: &str, single_start: &str) -> Count {
     let fmmap = match Mmap::open_path(filepath, Protection::Read) {
         Ok(mmap) => mmap,
         Err(e) => {
@@ -253,11 +270,102 @@ pub fn count_mmap_unsafe_single(filepath: &str, single_start: &str) -> Count {
     }
 }
 
-pub fn count_mmap_unsafe_multi(filepath: &str,
-                               single_start: &str,
-                               multi_start: &str,
-                               multi_end: &str)
-                               -> Count {
+pub fn count_multi_only(filepath: &str, multi_start: &str, multi_end: &str) -> Count {
+    // this is a duplicate of count_single_multi without the check for single comment.
+    // Basically removes one branch.  Probably pointless: benchmark.
+    let multiline_start = multi_start;
+    let multiline_end = multi_end;
+
+    let fmmap = match Mmap::open_path(filepath, Protection::Read) {
+        Ok(mmap) => mmap,
+        Err(e) => {
+            println!("mmap err for {}: {}", filepath, e);
+            return Count::default();
+        }
+    };
+    let bytes: &[u8] = unsafe { fmmap.as_slice() };
+
+    let mut lines = 0;
+    let mut code = 0;
+    let mut comments = 0;
+    let mut blanks = 0;
+
+    let mut in_comment = false;
+
+    let a = Ascii(bytes);
+    for byte_line in a.lines() {
+        let line = match std::str::from_utf8(byte_line) {
+            Ok(s) => s,
+            Err(_) => return Count::default(),
+        };
+        lines += 1;
+
+        let trimmed = line.trim_left();
+        if trimmed.is_empty() {
+            blanks += 1;
+            continue;
+        };
+
+
+        if !(trimmed.contains(multiline_start) || trimmed.contains(multiline_end)) {
+            if in_comment {
+                comments += 1;
+            } else {
+                code += 1;
+            }
+            continue;
+        }
+
+        let start_len = multiline_start.len();
+        let end_len = multiline_end.len();
+
+        let mut pos = 0;
+        let mut found_code = false;
+        'outer: while pos < trimmed.len() {
+            // TODO(cgag): must be a less stupid way to do this
+            for i in pos..(pos + cmp::max(start_len, end_len) + 1) {
+                if !trimmed.is_char_boundary(i) {
+                    pos += 1;
+                    continue 'outer;
+                }
+            }
+
+            if pos + start_len <= trimmed.len() &&
+               &trimmed[pos..pos + start_len] == multiline_start {
+                pos += start_len;
+                in_comment = true;
+            } else if pos + end_len <= trimmed.len() && &trimmed[pos..pos + end_len] == multiline_end {
+                pos += end_len;
+                in_comment = false;
+            } else if !in_comment {
+                found_code = true;
+                pos += 1;
+            } else {
+                pos += 1;
+            }
+        }
+
+        if found_code {
+            code += 1;
+        } else {
+            comments += 1;
+        }
+    }
+
+    Count {
+        code: code,
+        comment: comments,
+        blank: blanks,
+        lines: lines,
+    }
+}
+
+
+pub fn count_single_multi(filepath: &str,
+                          single_start: &str,
+                          multi_start: &str,
+                          multi_end: &str)
+                          -> Count {
 
     let single_line_start = single_start;
     let multiline_start = multi_start;
