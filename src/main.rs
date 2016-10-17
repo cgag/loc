01546e9af17b569e509dc395089869b7be213e16
Curtis Gagliardi <curtis@curtis.io>
2016-10-16 18:07:12 -0700

    implement parallelism as per ripgrep

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,51 @@
 extern crate count;
 
 extern crate clap;
-extern crate walkdir;
+extern crate deque;
 extern crate itertools;
+extern crate num_cpus;
+extern crate walkdir;
 
 use clap::{Arg, App, AppSettings};
 use walkdir::WalkDir;
 
 use std::collections::HashMap;
 use std::collections::hash_map::Entry;
+use std::thread;
+
+use deque::{Stealer, Stolen};
 
 use count as c;
+
+enum Work {
+    File(String),
+    Quit,
+}
+
+struct Worker {
+    chan: Stealer<Work>,
+}
+
+impl Worker {
+    fn run(self) -> Vec<(c::Lang, String, c::Count)> {
+        let mut v: Vec<(c::Lang, String, c::Count)> = vec![];
+        loop {
+            match self.chan.steal() {
+                // What causes these?
+                Stolen::Empty | Stolen::Abort => continue,
+                Stolen::Data(Work::Quit) => break,
+                Stolen::Data(Work::File(path)) => {
+                    let lang = c::lang_from_ext(&path);
+                    if lang != c::Lang::Unrecognized {
+                        let count = c::count(&path);
+                        v.push((lang, String::from(path), count));
+                    }
+                }
+            };
+        }
+        v
+    }
+}
 
 fn main() {
 
@@ -33,7 +68,14 @@ fn main() {
 
     let filepaths = matches.values_of("target").unwrap();
 
-    let mut counts: Vec<(c::Lang, String, c::Count)> = Vec::new();
+    let threads = num_cpus::get();
+    let mut workers = vec![];
+    let (workq, stealer) = deque::new();
+    for _ in 0..threads {
+        let worker = Worker { chan: stealer.clone() };
+        workers.push(thread::spawn(|| worker.run()));
+    }
+
     for filepath in filepaths {
         for entry in WalkDir::new(filepath) {
             let entry = match entry {
@@ -45,13 +87,18 @@ fn main() {
 
             if entry.file_type().is_file() {
                 let path = entry.path().to_str().unwrap();
-                let lang = c::lang_from_ext(path);
-                if lang != c::Lang::Unrecognized {
-                    let count = c::count(path);
-                    counts.push((lang, String::from(path), count));
-                }
+                workq.push(Work::File(String::from(path)));
             }
         }
+    }
+
+    for _ in 0..workers.len() {
+        workq.push(Work::Quit);
+    }
+
+    let mut counts: Vec<(c::Lang, String, c::Count)> = Vec::new();
+    for worker in workers {
+        counts.extend(worker.join().unwrap().iter().cloned())
     }
 
     let mut lang_counts_by_file: HashMap<c::Lang, Vec<(String, c::Count)>> = HashMap::new();
