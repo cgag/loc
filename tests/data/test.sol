731b3f530001d89e4ee3ffc8e1477d376f1600e9
Georgios Konstantopoulos <me@gakonst.com>
2019-08-29 13:27:47 +0300

    update readme and add test

---
+++ tests/data/test.sol
@@ -0,0 +1,16 @@
+pragma solidity ^0.4.0;
+
+// This is a contract
+contract SimpleStorage {
+    uint storedData;
+
+	// This is a setter
+    function set(uint x) public {
+        storedData = x;
+    }
+
+	// This is a getter
+    function get() public view returns (uint) {
+        return storedData;
+    }
+}
